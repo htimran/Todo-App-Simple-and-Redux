a260cd2ec6e2320fc4b9153e36a20b8dde7de782
Hafiz Imran <himran@thingtrax.com>
2021-07-29 15:10:32 +0500

    new changes for AWS

---
+++ build/precache-manifest.bbed0eea5786cbf7cf3aedd9348154b3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5b5c389abef216079852",
+    "url": "/static/css/main.7dd1f229.chunk.css"
+  },
+  {
+    "revision": "26eda2bbb6f41324fa305047fa8ced91",
+    "url": "/static/media/todomain.26eda2bb.png"
+  },
+  {
+    "revision": "4f2e9fd1f77dd964d1a3",
+    "url": "/static/js/1.4f2e9fd1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "5b5c389abef216079852",
+    "url": "/static/js/main.5b5c389a.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "4f2e9fd1f77dd964d1a3",
+    "url": "/static/css/1.83806848.chunk.css"
+  },
+  {
+    "revision": "10b1dc23f6ccc0eb6fd7bd20dd9bf529",
+    "url": "/index.html"
+  }
+];
